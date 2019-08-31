8afd1a32402b014d91bddda946e2f72d138a37af
betttyquu <absheng947@hotmail.com>
2019-08-30 22:49:16 -0700

    added slides to background

--- js/scripts.js
+++ js/scripts.js
@@ -94,4 +94,44 @@
         });
     });
 
+    // hide project name
+    // $('.project-hover').hide();
+    
+    // change on hover
+    $('.projectImage').on({
+        "mouseover" : function(e) {
+            // console.log('is this working?')
+            console.log('this', this.src);
+            e.preventDefault();
+            this.src = $('.projectImageChange');
+        },
+        "mouseout" : function(e) {
+            e.preventDefault();
+
+            console.log('mouseout',this.src)
+            this.src = $('.projectImage');
+        }
+    })
+
+// background slide
+//config
+var width = 720;
+var animationSpeed = 1000;
+var pause = 3000;
+var currentSlide = 1;
+
+//cache dom
+var $slider = $('#slider');
+var $slideContainer = $slider.find('.slides');
+var $slides = $slideContainer.find('.slide');
+setInterval(function() {
+    $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+        currentSlide++;
+        if (currentSlide === $slides.length) {
+            currentSlide = 1;
+            $slideContainer.css('margin-left', 0)
+        }
+    })
+}, pause);
+
 })(jQuery);
